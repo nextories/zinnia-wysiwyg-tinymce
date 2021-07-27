9e0147ddac7dd7257921c82343f1e38597382a68
Trang Dinh <trang.dinh@nextories.com>
2021-07-27 13:39:15 +0200

    change package for reverse, python_2_unicode_compatible

--- zinnia_tinymce/models.py
+++ zinnia_tinymce/models.py
@@ -6,7 +6,7 @@ import uuid
 
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
