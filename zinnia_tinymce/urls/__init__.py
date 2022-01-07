761b1fc5e3379ad2f577f7550a7433fc9c8babcc
Ludovic <ludovic.auberger@i-demenager.com>
2022-01-07 17:03:15 +0100

    Fix MimeType checking

--- zinnia_tinymce/urls/__init__.py
+++ zinnia_tinymce/urls/__init__.py
@@ -6,5 +6,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^', include('zinnia_tinymce.urls.links')),
     url(r'^filebrowser/', include('zinnia_tinymce.urls.filebrowser')),
-    url(r'^tinymce_js/', TemplateView.as_view(template_name='admin/zinnia/entry/tinymce_textareas.js'), name='tinymce-js'),
+    url(r'^tinymce_js/', TemplateView.as_view(template_name='admin/zinnia/entry/tinymce_textareas.js', content_type='application/javascript'), name='tinymce-js'),
 ]
